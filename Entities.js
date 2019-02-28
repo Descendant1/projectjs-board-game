11192ee4455b865c571a53e4026bc3e47fa4dbd2
descendant <kyrnacz@gmail.com>
2019-02-28 16:01:33 +0200

    Some battleField drawing

--- Entities.js
+++ Entities.js
@@ -1,3 +1,4 @@
+
 class Actor {
 
     constructor(id,type, attack, armor, health, attackSquares, speed)
@@ -16,19 +17,39 @@ class Actor {
     
     attack(target)
     {
-        if( ! target instanceof  Actor )
-            console.log('error')
-
-        console.log(this)
-        console.log('attacked ')
-        console.log(target)
-        
+        if( ! target instanceof  Actor || target._isDead)
+        {
+            alert('error')    
+            return;
+        }
+        var res =  throwDice();
+        var points  = this._attack - target._armor;
+        if(res == 3 )
+        {
+            points =  (points)/2;   
+            target._health -= points;
+        }
+        else if ( res  == target._health )
+        {
+            points = 0;
+        }
+        else 
+        {
+            target._health -= points;
+        }
+        if( target._health < 1  )
+        {
+            target._isDead = true;
+            target._available =  false;
+            console.log(target._type + ' is dead!');
+        }
+        game.getCurrentPlayer().updatePoints( points );
     }
     createSpan()
     {
         var span = document.createElement('span');
         span.id =  this._id;
-        span.innerText =  this._type;
+        span.innerText =  this._type + ' ' + this._health + ' health';
         span.style.display = 'block';
         span.style.backgroundColor = 'aquamarine';
         span.style.padding = '5px';
@@ -39,6 +60,11 @@ class Actor {
             game._selectedActor =  this;
         }
         return span;
+    }
+    getTextHolder()
+    {
+        this._textHolder.innerText = this._type + ' ' + this._health + ' health';
+        return this._textHolder;
     }
 }
 
@@ -101,9 +127,10 @@ class Game
     {
         this._firstPlayer   =  new Player(1,'Player1')
         this._secondPlayer  =  new Player(2,'Player2')
-        this._currentPlayer = this._firstPlayer;
-        this._selectedActor     = null;
-        this._board =  new Board('canvas','2d');
+        this._currentPlayer =  this._firstPlayer;
+        this._selectedActor =  null;
+        this._board         =  new Board('canvas','2d');
+        this._scoresBoard   =  document.getElementById('pScores');
 
     }
     getFirstPlayer () { return this._firstPlayer }
@@ -118,10 +145,11 @@ class Game
         setInterval(()=>{
             
             this.getCurrentPlayer()._pointsTextHolder.innerText       =  `${this.getCurrentPlayer()._playerName} current points = ${this.getCurrentPlayer()._currentPoints}`;
-        //  this.getCurrentPlayer().getActors(i=>!i._isDead).map(i=> this.getCurrentPlayer()._availActorsTextHolder.appendChild(i._textHolder)) ;
+            Array.from(document.querySelectorAll('div#pActors span')).map(i=>i.remove())
+            this.getCurrentPlayer().getActors(i=>!i._isDead && i._available ).map(i=> this.getCurrentPlayer()._availActorsTextHolder.appendChild(i.getTextHolder())) ;
+            this._scoresBoard.innerHTML = `${this.getFirstPlayer()._playerName} score is ${this.getFirstPlayer()._currentPoints } &nbsp; ${this.getSecondPlayer()._playerName} score is ${this.getSecondPlayer()._currentPoints }`;
             
         },500)
-        this.getCurrentPlayer().getActors(i=>!i._isDead).map(i=> this.getCurrentPlayer()._availActorsTextHolder.appendChild(i._textHolder)) ;
 
     }
 }
@@ -132,10 +160,32 @@ class Board
 {
     constructor(canvasId, dimension)
     {
-        this._boardWidth        = 9;
-        this._boardHeight       = 7;
-        this._totalBoardCells   = this._boardHeight * this._boardWidth; 
+        this._boardWidth        = 900;
+        this._boardHeight       = 700;
+        this._boardWidthSize    = 9;
+        this._boardHeightSize   = 7;
+        this._totalBoardCells   = this._boardWidthSize * this._boardHeightSize; 
         this._canvas            = document.getElementById(canvasId);
+            this._canvas.width      = this._boardWidth;
+            this._canvas.height     = this._boardHeight;
+            this._canvas.onclick    = (e) => 
+                                            {
+                                                var rect = this.collides( e.offsetX, e.offsetY);                                                
+                                                if(game._selectedActor != null && !rect._currentActor )
+                                                {
+                                                    if(rect._positionInheritance === 'battleField')  { return; }
+
+                                                    if (rect) 
+                                                    {
+                                                        rect._currentActor  =  game._selectedActor;
+                                                        game._selectedActor._available =  false;
+                                                        game._selectedActor = null;
+                                                        console.log(rect);
+                                                    } 
+                                                    else { return; }
+                                                }
+                                                else {  console.log('Клетка занята или не выбран хуйкин') }
+                                            }    
         this._context           = this._canvas.getContext(dimension);
         this._cells             = [];
         this.init();
@@ -146,31 +196,82 @@ class Board
     }
     initializeBoard () 
     {
-        this.fillCells();
-    }
-    fillCells()
-    {
+        let squareWidth = this._boardWidth / this._boardWidthSize;
+        let squareHeight = this._boardHeight / this._boardHeightSize;
+        let totalSquares = this._totalBoardCells;
+        let i, x, y = -1;
+        let battleField = null;
+                
+        for (i = 0; i < totalSquares; i++) {
+          x++;
+          if (i % this._boardWidthSize == 0) {
+            y++; 
 
+            x = 0;
+          }     
+          if(y>=2 && y <=4)
+            battleField =  'battleField';
+          let tempCell =  new Cell(x * squareWidth, y * squareWidth, squareWidth, squareHeight, battleField);
+          battleField = null;
+          this.push(tempCell) ? tempCell.drawRectangleCell(this._context,x,y) : alert('error');
+        }
     }
-    
+    push(tempCell)
+    {
+       if( ! tempCell instanceof  Cell) { return false; }
+       this._cells.push(tempCell);
+       return true;
+    }
+    collides(x, y) 
+    {
+            var isCollision = false;
+            for (var i = 0 ; i < this._cells.length; i++) {
+                let left = this._cells[i]._x , 
+                    right  = this._cells[i]._x + this._cells[i]._width;
+
+                let top =  this._cells[i]._y , 
+                    bottom = this._cells[i]._y + this._cells[i]._height;
+                if ( right >= x && left <= x
+                     && bottom >= y && top <= y) 
+                {
+                    isCollision = this._cells[i];
+                }
+            }
+            return isCollision;
+    }
 }
 
 
 class Cell 
 {
-    constructor()
+    constructor(x,y,width,height, inh)
     {
-        this._id = 1 + this._x * 1 + this._y
+        this._id =  getRandomNext(1,100000)- (width - x) ;
         this._status = '';
-        this._positionInheritence =  '';
+        this._positionInheritance =  inh? inh : null;
         this._currentActor  = null;
-        this._x = x;
-        this._y = y;
-
+        this._x         = x;
+        this._y         = y;
+        this._width     =  width;
+        this._height    =  height;
+        this.options            = {
+            light: '#d1eefc',
+            dark : '#1f1f21'
+            }   
     }
+
+    drawRectangleCell(ctx,x,y){
+        ctx.beginPath();
+        ctx.rect(this._x, this._y, this._width, this._height);
+        if(this._positionInheritance != null ){
+            ctx.fillStyle = (x + y) % 2 ?  '#0015ff':'#ff0000';
+        }
+        else {ctx.fillStyle = (x + y) % 2 ? this.options.dark : this.options.light; }
+        ctx.fill();
+    }
+
 }
 
 const game =  new Game();
 game.render();
-var p1 = game.getFirstPlayer();
-var p2 =  game.getSecondPlayer();
+
--- Utils.js
+++ Utils.js
@@ -2,3 +2,7 @@ getRandomNext = (min, max) =>
 {
     return parseInt(Math.random() * (max - min) + min);
 }
+throwDice = () =>
+{
+    return getRandomNext(1,6) + getRandomNext(1,6) + getRandomNext(1,6);    
+}
